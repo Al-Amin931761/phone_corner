33efa406affd46dc7990caddd3ed3f8d4a273e05
Al-Amin931761 <alamin931761@gmail.com>
2022-03-02 14:52:13 +0600

    background color

--- js/index.js
+++ js/index.js
@@ -43,13 +43,13 @@ const displaySearchResult = phones => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-              <div class="card h-100 shadow p-3 mb-5 bg-body rounded">
+              <div class="card h-100 shadow p-3 mb-5 card-background rounded ">
                           <img src="${phone.image}" class="card-img-top" alt="...">
                       <div class="card-body">
-                  <h5 class="card-title"><span class ="fw-bold">Name:</span> ${phone.phone_name}</h5>
-                  <p class="card-text"><span class ="fw-bold">Brand:</span> ${phone.brand}</p>
+                  <p class="card-title text-center fs-2 mt-2"><span class ="fw-bold">Name:</span> ${phone.phone_name}</p>
+                  <p class="card-text text-center fs-3 mt-4"><span class ="fw-bold ">Brand:</span> ${phone.brand}</p>
                       </div>
-                      <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-info">Details</button>
+                      <a class="details-button" onclick="loadPhoneDetails('${phone.slug}')" href="#phone">Details</a>
               </div>
               `;
             searchResult.appendChild(div);
@@ -67,7 +67,7 @@ const loadPhoneDetails = phoneId => {
 }
 
 const displayPhoneDetails = phone => {
-    // console.log(phone.name);
+    console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = '';
 
@@ -76,55 +76,59 @@ const displayPhoneDetails = phone => {
 
     if (phone.releaseDate == '') {
         div.innerHTML = `
-    <img src="${phone.image}" class="card-img-top p-5" alt="...">
-        <div class="card-body">
-            <h5 class="card-title"><span class ="fw-bold">Name:</span> ${phone.name}</h5>
-            <p class="card-text"><span class ="fw-bold">Brand:</span> ${phone.brand}</p>
-            <p class="card-text"><span class ="fw-bold">Brandy:</span> ${phone.brand}</p>
-            <p><span class ="fw-bold">Release Date: </span>No release date found</p>
-    <div>
-        <p class="text-center fw-bold fs-5 text-info"> Main Features:<p>
-            <p><span class="fw-bold">Chipset:</span> ${phone.mainFeatures.chipSet} </p>
-            <p><span class="fw-bold">Display Size:</span> ${phone.mainFeatures.displaySize} </p>
-            <p><span class="fw-bold">Memory:</span> ${phone.mainFeatures.memory} </p>
-            <p><span class="fw-bold">Storage:</span> ${phone.mainFeatures.storage} </p>
-            <p><span class="fw-bold">Sensors:</span> ${phone.mainFeatures.sensors[0]},${phone.mainFeatures.sensors[1]},${phone.mainFeatures.sensors[2]},${phone.mainFeatures.sensors[3]},${phone.mainFeatures.sensors[4]},${phone.mainFeatures.sensors[5]}</p>
+   <div class="details-card-background" id ="phone">
+   <img src="${phone.image}" class="card-img-top p-5" alt="...">
+   <div class="card-body">
+       <h5 class="card-title"><span class ="fw-bold">Name:</span> ${phone.name}</h5>
+       <p class="card-text"><span class ="fw-bold">Brand:</span> ${phone.brand}</p>
+       <p class="card-text"><span class ="fw-bold">Brandy:</span> ${phone.brand}</p>
+       <p><span class ="fw-bold">Release Date: </span>No release date found</p>
+<div>
+   <p class="text-center fw-bold fs-5 text-info"> Main Features:<p>
+       <p><span class="fw-bold">Chipset:</span> ${phone.mainFeatures.chipSet} </p>
+       <p><span class="fw-bold">Display Size:</span> ${phone.mainFeatures.displaySize} </p>
+       <p><span class="fw-bold">Memory:</span> ${phone.mainFeatures.memory} </p>
+       <p><span class="fw-bold">Storage:</span> ${phone.mainFeatures.storage} </p>
+       <p><span class="fw-bold">Sensors:</span> ${phone.mainFeatures.sensors[0]},${phone.mainFeatures.sensors[1]},${phone.mainFeatures.sensors[2]},${phone.mainFeatures.sensors[3]},${phone.mainFeatures.sensors[4]},${phone.mainFeatures.sensors[5]}</p>
 
-            <p class="text-center fw-bold fs-5 text-info">Others:</p>
-            <p><span class="fw-bold">Bluetooth:</span> ${phone.others.Bluetooth} </p>
-            <p><span class="fw-bold">GPS:</span> ${phone.others.GPS} </p>
-            <p><span class="fw-bold">NFC:</span> ${phone.others.NFC} </p>
-            <p><span class="fw-bold">Radio:</span> ${phone.others.Radio} </p>
-            <p><span class="fw-bold">USB:</span> ${phone.others.USB} </p>
-            <p><span class="fw-bold">WLAN:</span> ${phone.others.WLAN} </p>
-        </div>
-    </div>
+       <p class="text-center fw-bold fs-5 text-info">Others:</p>
+       <p><span class="fw-bold">Bluetooth:</span> ${phone.others.Bluetooth} </p>
+       <p><span class="fw-bold">GPS:</span> ${phone.others.GPS} </p>
+       <p><span class="fw-bold">NFC:</span> ${phone.others.NFC} </p>
+       <p><span class="fw-bold">Radio:</span> ${phone.others.Radio} </p>
+       <p><span class="fw-bold">USB:</span> ${phone.others.USB} </p>
+       <p><span class="fw-bold">WLAN:</span> ${phone.others.WLAN} </p>
+   </div>
+</div>
+   </div>
 `;
     }
     else {
         div.innerHTML = `
+    <div class="details-card-background" id="phone">
     <img src="${phone.image}" class="card-img-top p-5" alt="...">
-        <div class="card-body">
-            <h5 class="card-title"><span class ="fw-bold">Name:</span> ${phone.name}</h5>
-            <p class="card-text"><span class ="fw-bold">Brand:</span> ${phone.brand}</p>
-            <p><span class ="fw-bold">Release Date:</span> ${phone.releaseDate}</p>
-    <div>
-        <p class="text-center fw-bold fs-5 text-info"> Main Features:<p>
-            <p><span class="fw-bold">Chipset:</span> ${phone.mainFeatures.chipSet} </p>
-            <p><span class="fw-bold">Display Size:</span> ${phone.mainFeatures.displaySize} </p>
-            <p><span class="fw-bold">Memory:</span> ${phone.mainFeatures.memory} </p>
-            <p><span class="fw-bold">Storage:</span> ${phone.mainFeatures.storage} </p>
-            <p><span class="fw-bold">Sensors:</span> ${phone.mainFeatures.sensors[0]},${phone.mainFeatures.sensors[1]},${phone.mainFeatures.sensors[2]},${phone.mainFeatures.sensors[3]},${phone.mainFeatures.sensors[4]},${phone.mainFeatures.sensors[5]}</p>
+    <div class="card-body">
+        <h5 class="card-title"><span class ="fw-bold">Name:</span> ${phone.name}</h5>
+        <p class="card-text"><span class ="fw-bold">Brand:</span> ${phone.brand}</p>
+        <p><span class ="fw-bold">Release Date:</span> ${phone.releaseDate}</p>
+<div>
+    <p class="text-center fw-bold fs-5 text-info"> Main Features:<p>
+        <p><span class="fw-bold">Chipset:</span> ${phone.mainFeatures.chipSet} </p>
+        <p><span class="fw-bold">Display Size:</span> ${phone.mainFeatures.displaySize} </p>
+        <p><span class="fw-bold">Memory:</span> ${phone.mainFeatures.memory} </p>
+        <p><span class="fw-bold">Storage:</span> ${phone.mainFeatures.storage} </p>
+        <p><span class="fw-bold">Sensors:</span> ${phone.mainFeatures.sensors[0]},${phone.mainFeatures.sensors[1]},${phone.mainFeatures.sensors[2]},${phone.mainFeatures.sensors[3]},${phone.mainFeatures.sensors[4]},${phone.mainFeatures.sensors[5]}</p>
 
-            <p class="text-center fw-bold fs-5 text-info">Others:</p>
-            <p><span class="fw-bold">Bluetooth:</span> ${phone.others.Bluetooth} </p>
-            <p><span class="fw-bold">GPS:</span> ${phone.others.GPS} </p>
-            <p><span class="fw-bold">NFC:</span> ${phone.others.NFC} </p>
-            <p><span class="fw-bold">Radio:</span> ${phone.others.Radio} </p>
-            <p><span class="fw-bold">USB:</span> ${phone.others.USB} </p>
-            <p><span class="fw-bold">WLAN:</span> ${phone.others.WLAN} </p>
-        </div>
+        <p class="text-center fw-bold fs-5 text-info">Others:</p>
+        <p><span class="fw-bold">Bluetooth:</span> ${phone.others.Bluetooth} </p>
+        <p><span class="fw-bold">GPS:</span> ${phone.others.GPS} </p>
+        <p><span class="fw-bold">NFC:</span> ${phone.others.NFC} </p>
+        <p><span class="fw-bold">Radio:</span> ${phone.others.Radio} </p>
+        <p><span class="fw-bold">USB:</span> ${phone.others.USB} </p>
+        <p><span class="fw-bold">WLAN:</span> ${phone.others.WLAN} </p>
     </div>
+</div>
+</div>
 `;
     }
     phoneDetails.appendChild(div);
